016701ffeea667f8cbd68868f33e4dd96fac097e
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-10-28 20:18:39 +0200

    Create mempool_change.py

---
+++ mempool_change.py
@@ -0,0 +1,42 @@
+import time
+from bitcoinrpc.authproxy import AuthServiceProxy#, JSONRPCException
+from config import config as c
+
+
+def main():
+    rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(c.rpc_user, c.rpc_password))
+
+    mempool_info = rpc_connection.getmempoolinfo()
+    mempool_info_loaded = mempool_info['loaded']
+    #print(f'{mempool_info=} {type(mempool_info)=}')
+    #print(f'{mempool_info=} {type(mempool_info)=}')
+    if mempool_info_loaded is False:
+        print('Still waiting to load mempool after restart of bitcoin-core')
+        print(f'{mempool_info=} {type(mempool_info)=}')
+        exit()
+
+    previous_txids = set()
+    while True:
+        all_txids = set(rpc_connection.getrawmempool())
+
+        #print(f'{all_txids=} {type(all_txids)=}')
+        #print(f'{len(all_txids)=} {type(all_txids)=}')
+
+        if previous_txids:
+            added_txids = all_txids - previous_txids
+            removed_txids = previous_txids - all_txids
+
+            print(f'{len(all_txids)=:6} {len(added_txids)=:6} {len(removed_txids)=:6}')
+            if len(removed_txids) > 0:
+                print(f'    {removed_txids=}')
+                print(f'    TRANSACTION REMOVED: {len(removed_txids)=}')
+                # need to check why each transaction was removed
+        else:
+            print('FIRST in loop')
+
+        previous_txids = all_txids
+        time.sleep(28)  # 30 not working, default timeout in AuthServiceProxy
+
+
+if __name__ == "__main__":
+    main()
