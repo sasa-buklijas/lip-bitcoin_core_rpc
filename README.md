99bef28ed5e6ef91890b9e0c247547f4b560aa7e
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-10-31 20:03:14 +0100

    new_block_notification.py

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # lip-bitcoin_core_rpc
- 
+
+## Requirements
+Developed with Python:
+ - [version](https://github.com/sasa-buklijas/lip-bitcoin_core_rpc/blob/main/.python-version)
+ - [packages](https://github.com/sasa-buklijas/lip-bitcoin_core_rpc/blob/main/requirements.txt)
+
+## Code Examples
+### [new_block_notification.py](https://github.com/sasa-buklijas/lip-bitcoin_core_rpc/blob/main/new_block_notification.py)  
+Uses bitcon-core ZeroMQ(zmqpubhashblock) to monitor for new blocks.  
+On OSX(macOS) notification is displayed on screen with block statistic.  
+
+### [getbestblockhash.py](https://github.com/sasa-buklijas/lip-bitcoin_core_rpc/blob/main/getbestblockhash.py)  
+Hello world example for bitcon-core RPC.
+
+### [mempool_change.py](https://github.com/sasa-buklijas/lip-bitcoin_core_rpc/blob/main/mempool_change.py)  
+Polling bitcoin-core mempool via RPC(getmempoolinfo) to calculate difference between added and removed txids.  
+If pooling is to slow, use [bitcoin-core ZeroMQ streams](https://bitcoindev.network/accessing-bitcoins-zeromq-interface/). 
+
+
---
+++ new_block_notification.py
@@ -0,0 +1,89 @@
+import zmq
+import struct
+import platform
+import os
+import time
+#from datetime import datetime, UTC
+import datetime as dt
+from bitcoinrpc.authproxy import AuthServiceProxy
+from config import config as c
+
+
+def main():
+    my_os = platform.system()
+
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://127.0.0.1:29000")  
+    # Subscribe to "pubhashblock" messages
+    socket.setsockopt_string(zmq.SUBSCRIBE, "hashblock")
+
+    print("Subscribed to pubhashblock messages. Waiting for messages...")
+    previous_block_time = None
+    while True:
+        topic, body, seq = socket.recv_multipart()
+        current_time = time.time()
+        #print(f'{type(topic)=} {type(body)=} {type(seq)=}')
+        #print(f'{topic=} {body=} {seq=}')
+        sequence = str(struct.unpack('<I', seq)[-1])
+
+        print('- HASH BLOCK ('+sequence+') -')
+        print(body.hex())
+
+        rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(c.rpc_user, c.rpc_password))
+        block = rpc_connection.getblock(body.hex())
+        stats = rpc_connection.getblockstats(body.hex())
+        print(f'{str(block)[0:1000]}')
+        print(stats)
+        block_height = block['height']
+        block_nTx = block['nTx']
+        block_MB = float(block['size'])/1024/1024
+        block_weight = float(block['weight'])/4_000_000*100
+        block_time = block['time']
+        block_mediantime = block['mediantime']
+        #print(f'{current_time=} - {datetime.utcfromtimestamp(current_time).strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{current_time=} - {dt.datetime.fromtimestamp(current_time, dt.UTC).strftime("%Y-%m-%d %H:%M:%S")}')
+        #print(f'{block_time=} - {datetime.utcfromtimestamp(block_time).strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{block_time=} - {dt.datetime.fromtimestamp(block_time, dt.UTC).strftime("%Y-%m-%d %H:%M:%S")}')
+        #print(f'{block_mediantime=} - {datetime.utcfromtimestamp(block_mediantime).strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{block_mediantime=} - {dt.datetime.fromtimestamp(block_mediantime, dt.UTC).strftime("%Y-%m-%d %H:%M:%S")}')
+        min_fee_rate = stats['minfeerate']
+        median_fee_rate = stats['feerate_percentiles'][2]
+        avg_fee_rate = stats['avgfeerate']
+
+        #print(f'{block_MB=} {block_weight=} ')
+        block_template = ''
+        if block_nTx == 1:
+            block_template = '- IS_TEMPLATE '
+
+        #if previous_block_time is None:
+        #    seconds_since_previous_block = current_time - block_time 
+        #else:
+        #    seconds_since_previous_block = current_time - previous_block_time
+
+        # human readable time since previous block
+        human_time_since_previous_block = ''
+        if previous_block_time:
+            seconds_since_previous_block = current_time - previous_block_time
+
+            if seconds_since_previous_block < 60:  
+                human_time_since_previous_block = f'- {seconds_since_previous_block:.0f} sec' 
+            else:
+                minutes = seconds_since_previous_block // 60
+                seconds = seconds_since_previous_block % 60
+                human_time_since_previous_block = f'- {minutes:.0f} min {seconds:.0f} sec'
+
+        # OS specific notification 
+        if my_os == 'Darwin':  # OSX
+            title = f'Block {block_height} {human_time_since_previous_block}'
+            msg = f'{block_MB:.2f} MB ({block_weight:.2f}%) - {block_nTx} tx {block_template}- {min_fee_rate}/{median_fee_rate}/{avg_fee_rate} sat/vB'
+            print(f'{title=}')
+            print(f'{msg=}')
+            print()
+            os.system(f"osascript  -e 'display notification \"{msg}\" with title \"{title}\"'")
+
+        previous_block_time = current_time
+
+
+if __name__ == "__main__":
+    main()
